f8817621df6adf7e5e904b55aa0cc0058a37a570
Feresey <p.milko1999@yandex.ru>
2020-01-21 17:35:35 +0300

    todo

--- db/api.go
+++ db/api.go
@@ -1,15 +1,17 @@
 package db
 
-import "github.com/jackc/pgx/v4"
+import (
+	"context"
 
-import "context"
+	"github.com/jackc/pgx/v4"
+)
 
 func QueryRow(q string, args ...interface{}) pgx.Row {
 	log.Info("query:", q)
-	return db.QueryRow(context.Background(), q, args...)
+	return db.QueryRow(context.TODO(), q, args...)
 }
 
 func Query(q string, args ...interface{}) (pgx.Rows, error) {
 	log.Info("query:", q)
-	return db.Query(context.Background(), q, args...)
+	return db.Query(context.TODO(), q, args...)
 }
