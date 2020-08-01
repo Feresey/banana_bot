996cccbe7b58fc3f5a4592b70d564a3f7cf8c24f
Feresey <p.milko1999@yandex.ru>
2020-08-01 18:31:57 +0300

    go

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -176,7 +176,7 @@ func (b *Bot) Start() {
 	s := <-sig
 	b.log.Info("Signal received", zap.Stringer("signal", s))
 
-	go b.handleText(&tgbotapi.Message{Chat: &tgbotapi.Chat{ID: godID}}) //nolint:errcheck
+	b.handleText(&tgbotapi.Message{Chat: &tgbotapi.Chat{ID: godID}}) //nolint:errcheck
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
